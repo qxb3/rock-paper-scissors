e8ef8ce5efcb7e060c7395b715392e882692db3a
qxb3 <toolong972@gmail.com>
2022-03-06 15:22:13 +0800

    Kinda finished game

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,81 @@
 <script>
-	export let name;
+	const images = {
+		rock: {
+			src: 'images/rock.png',
+			alt: 'rock'
+		},
+		paper: {
+			src: 'images/paper.png',
+			alt: 'paper'
+		},
+		scissors: {
+			src: 'images/scissors.png',
+			alt: 'scissors'
+		}
+	}
+
+	let playerImage = images.rock
+	let enemyImage = images.rock
+
+	const randomNumber = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
+
+	//Scores
+	let playerScore = 0
+	let enemyScore = 0
+
+	const updatePlayer = (button) => {
+		if (button === 'rock') playerImage = images.rock
+		if (button === 'paper') playerImage = images.paper
+		if (button === 'scissors') playerImage = images.scissors
+	}
+
+	const updateEnemy = (num) => {
+		if (num === 0) enemyImage = images.rock
+		if (num === 1) enemyImage = images.paper
+		if (num === 2) enemyImage = images.scissors
+	}
+
+	const updateScore = (player, enemy) => {
+		if (player === 'rock' && enemy === 'scissors') playerScore++
+		if (player === 'paper' && enemy === 'rock') playerScore++
+		if (player === 'scissors' && enemy === 'paper') playerScore++
+
+		if (enemy === 'rock' && player === 'scissors') enemyScore++
+		if (enemy === 'paper' && player === 'rock') enemyScore++
+		if (enemy === 'scissors' && player === 'paper') enemyScore++
+	}
+
+	const buttonClick = (button) => {
+		updatePlayer(button)
+		updateEnemy(randomNumber(0, 2))
+		updateScore(playerImage.alt, enemyImage.alt)
+	}
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2>Score: {playerScore} - {enemyScore}</h2>
+	<div id='game'>
+
+		<div>
+			<h1>Player</h1>
+			<img src='{playerImage.src}' alt='{playerImage.alt}' width='64px' height='64px'>
+		</div>
+
+		<div>
+			<h1>Enemy</h1>
+			<img src='{enemyImage.src}' alt='{enemyImage.alt}' width='64px' height='64px'>
+		</div>
+
+	</div>
+
+	<div id='buttons'>
+		<button on:click={() => buttonClick('rock')}>Rock</button>
+		<button on:click={() => buttonClick('paper')}>Paper</button>
+		<button on:click={() => buttonClick('scissors')}>Scissor</button>
+	</div>
 </main>
 
 <style>
@@ -15,16 +86,13 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	#game {
+		display: grid;
+		grid-template-columns: repeat(2, auto);
+		grid-column-end: span;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	#buttons {
+		margin-top: 64px;
 	}
 </style>
